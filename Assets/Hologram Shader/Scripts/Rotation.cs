c32d9102c005065c6a0e572a0dec7749ab46a773
Jonathan Gonzalez <gonzo@cgcookie.com>
2019-08-25 17:53:35 -0700

    Added Shader Example Assets
    Added hologram shader with animated scan lines and sample model assets to demonstrate how these work.

---
+++ Assets/Hologram Shader/Scripts/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float rotationSpeed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed);
+    }
+}
